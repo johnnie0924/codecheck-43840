667f77f52f6e5a8740a3a9c8241339f72f1583a9
akiyama_kazuhiko <akiyama_kazuhiko@192.168.11.5>
2018-06-20 20:51:58 +0900

    commit

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -7,24 +7,25 @@ public class App {
 			System.out.println(output);
 		}
 	}
+
 	public static String check(String inStr) {
 		String result = "";
 		try {
 			int inInt = Integer.parseInt(inStr);
 
-			if(inInt > 1000 || inInt < 0) {
+			if (inInt > 1000 || inInt < 0 || inStr.contains("０") || inStr.contains("０") || inStr.contains("１")
+					|| inStr.contains("２") || inStr.contains("３") || inStr.contains("４") || inStr.contains("５")
+					|| inStr.contains("６") || inStr.contains("７") || inStr.contains("８") || inStr.contains("９")) {
 				result = "invalid";
-			}else if(inInt % 3 == 0 && inStr.contains("3")){
+			} else if (inInt % 3 == 0 && inStr.contains("3")) {
 				result = "dumb";
-			}else if(inInt % 3 == 0){
+			} else if (inInt % 3 == 0) {
 				result = "idiot";
-			}else if(inStr.contains("3")) {
+			} else if (inStr.contains("3")) {
 				result = "stupid";
-			}else {
+			} else {
 				result = "smart";
 			}
-
-
 
 		} catch (NumberFormatException e) {
 			e.printStackTrace();
